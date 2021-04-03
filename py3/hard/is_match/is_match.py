7eaee5abbecc9713274783e27aed4e5056b5a0df
Dmitry Kossovich <redmouce@ya.ru>
2021-04-03 03:58:35 +0300

    add new solution. didn't pass all tests yet

---
+++ py3/hard/is_match/is_match.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def fit(self, s, re_piece, offset, is_next_star):
+        for i, c in enumerate(re_piece):
+            if not (
+                c == "." or
+                s[i + offset] == "." or
+                c == s[i + offset] or
+                (i == len(re_piece) - 1 and is_next_star)
+            ):
+                return False
+        return True
+
+    def check_match(self, s, rg, rg_i, offset=0):
+        if rg_i == len(rg):
+            return len(s) == offset
+        while offset < len(s):
+            if self.fit(s, rg[rg_i][0], offset, rg_i + 1 < len(rg)):
+                offset += max(len(rg[rg_i][0]), 1)
+                return self.check_match(s, rg, rg_i+1, offset)
+            elif offset + 1 < len(s) and s[offset+1] == rg[rg_i][1]:
+                offset += 1
+            else:
+                return False
+        if offset == len(s):
+            return False
+
+    def isMatch(self, s, p):
+        rg = []
+        p_sp = p.split("*")
+        for i, r in enumerate(p_sp):
+            rg.append([r, p_sp[i-1][len(p_sp[i-1])-1] if i else "-"])
+
+        return self.check_match(s, rg, 0)
+
+
+def main():
+    a = input()
+    b = input()
+    print(Solution().isMatch(a, b))
+
+
+if __name__ == "__main__":
+    main()
